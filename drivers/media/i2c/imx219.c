4d3023751712ce73b676f0ab48f3471e88c30782
ge35huz <michael.berghold@oewf.org>
2026-09-02 22:52:43 +0200

    .

--- drivers/media/i2c/imx219.c
+++ drivers/media/i2c/imx219.c
@@ -563,10 +563,13 @@ static int imx219_write_reg(struct imx219 *imx219, u16 reg, u32 len, u32 val)
 	if (len > 4)
 		return -EINVAL;
 
-	if (reg != 0x0100)
-		return 0;
 
-	printk(KERN_INFO "Would write to reg %04X value %08X (%d) on addr 0x%02X", reg, val, len, client->addr);
+	if (reg != 0x0100) {
+		printk(KERN_INFO "Would write to reg %04X value %08X (%d) on addr 0x%02X", reg, val, len, client->addr);
+		return 0;
+	}
+
+	printk(KERN_INFO "Actually writing to reg %04X value %08X (%d) on addr 0x%02X", reg, val, len, client->addr);
 
 	put_unaligned_be16(reg, buf);
 	put_unaligned_be32(val << (8 * (4 - len)), buf + 2);
